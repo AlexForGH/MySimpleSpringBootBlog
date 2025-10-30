34e21cd90aa8e8c6d7cc9a9cafbb006b1de8db0c
AlexForGH <for_git_hub@mail.ru>
2025-10-30 15:43:45 +0300

    add models and dtos

---
+++ src/main/java/org/simple_spring_boot_blog/dto/PagingInfoDto.java
@@ -0,0 +1,10 @@
+package org.simple_spring_boot_blog.dto;
+
+public record PagingInfoDto(
+        int pageNumber,
+        int totalPages,
+        int pageSize,
+        boolean hasPrevious,
+        boolean hasNext
+) {
+}
