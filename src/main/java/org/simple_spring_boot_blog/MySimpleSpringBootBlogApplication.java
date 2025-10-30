f678b96639f522a8a027dc1be08ca2fc29b2a9df
AlexForGH <for_git_hub@mail.ru>
2025-10-30 16:05:43 +0300

    add in main class multipartResolver bean, uploadPath and ResourceHandler

--- src/main/java/org/simple_spring_boot_blog/MySimpleSpringBootBlogApplication.java
+++ src/main/java/org/simple_spring_boot_blog/MySimpleSpringBootBlogApplication.java
@@ -1,13 +1,30 @@
 package org.simple_spring_boot_blog;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MySimpleSpringBootBlogApplication {
+public class MySimpleSpringBootBlogApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySimpleSpringBootBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MySimpleSpringBootBlogApplication.class, args);
+    }
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("file:///" + uploadPath);
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
