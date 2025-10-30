9851a9d25eecf2dc0b8af0ee1e7d363dbb9eb5e7
AlexForGH <for_git_hub@mail.ru>
2025-10-30 15:05:37 +0300

    init the project

---
+++ src/main/java/org/simple_spring_boot_blog/MySimpleSpringBootBlogApplication.java
@@ -0,0 +1,13 @@
+package org.simple_spring_boot_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleSpringBootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySimpleSpringBootBlogApplication.class, args);
+	}
+
+}
